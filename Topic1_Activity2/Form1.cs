8abe4aa0fe66ce6b9e8c47c838736d8105d9a219
Sambro09 <116610371+Sambro09@users.noreply.github.com>
2023-03-16 14:01:01 -0700

    Updated for Activity 6, added catch system to make sure only avalible input is numbers.

--- Topic1_Activity2/Form1.cs
+++ Topic1_Activity2/Form1.cs
@@ -9,12 +9,18 @@ namespace Topic1_Activity2
 
         private void convertButton_Click(object sender, EventArgs e)
         {
-            double celsius = double.Parse(celsiusInput.Text);
+            double celsius;
+            if (double.TryParse(celsiusInput.Text, out celsius))
+            {
+                //formula (0°C × 9/5) + 32 = 32°F
+                double fahrenhiet = (celsius * 9 / 5) + 32;
 
-            //formula (0°C × 9/5) + 32 = 32°F
-            double fahrenhiet = (celsius * 9 / 5) + 32;
-
-            fahrenhietOutput.Text = fahrenhiet.ToString();
+                fahrenhietOutput.Text = fahrenhiet.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input");
+            }
         }
     }
 }
